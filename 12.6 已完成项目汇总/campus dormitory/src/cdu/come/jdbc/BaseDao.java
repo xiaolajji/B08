d939414cee0d633d987c878dfc4879a4ca582b97
xiaolajji <92966570+xiaolajji@users.noreply.github.com>
2021-12-06 13:37:50 +0800

    Add files via upload

---
+++ 12.6 已完成项目汇总/campus dormitory/src/cdu/come/jdbc/BaseDao.java
@@ -0,0 +1,34 @@
+package cdu.come.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDao {
+	private String dbUrl="jdbc:mysql://localhost:3306/sushe";
+	private String dbUser="root";
+	private String dbPassword="123456";
+	private String jdbcName="com.mysql.cj.jdbc.Driver";
+	
+	//连接数据库
+	public Connection getConn(){
+		Connection conn = null;
+		try{
+			Class.forName(jdbcName);
+		}
+		catch(Exception e){}
+		try{
+			conn=DriverManager.getConnection(dbUrl,dbUser,dbPassword);
+		}
+		catch(SQLException ex){}
+		return conn;		
+	}
+	
+//    测试
+	public static void main(String[] args)
+	{
+		System.out.println(new BaseDao().getConn());
+		
+	}
+	
+}
